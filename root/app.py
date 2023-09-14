6d98511a90df942b1a73f81f839918d121129c64
tommychanchan <41499833+tommychanchan@users.noreply.github.com>
2023-09-14 20:13:22 +0800

    stock_info for multiple stocks

--- root/app.py
+++ root/app.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, jsonify, request
 import yfinance as yf
+import requests
 
 app = Flask(__name__)
 
@@ -21,31 +22,41 @@ def index():
 # get stock information
 # -- parameters --
 # stock: the stock id (e.g. "9988.hk", "tsla")
-# -- return keys --
-# currency: "HKG"/"USD"/"TWD"
-# currentPrice: float
+# -- return --
+# a list of stock info
 # -- error messages --
-# 1: argument stock missing
-# 2: stock id not found
-@app.route('/stock_info')
+# 1: no stock provided
+# -- error messages in each element --
+# 2: cannot connect to server(yahoo api server)
+# 3: stock id not found
+@app.route('/stock_info', methods=['POST'])
 def stock_info():
-    stock = request.args.get('stock')
-    if stock == None:
+    data = [s.upper() for s in request.json]
+    # data: a list of stock names (uppercase)
+
+    if len(data) == 0:
         return jsonify({
             'error': 1,
         })
 
-    try:
-        ticker = yf.Ticker(stock).info
-    except:
-        return jsonify({
-            'error': 2,
-        })
+    return_list = []
+    tickers = yf.Tickers(' '.join(data))
 
+    for stock_name in data:
+        try:
+            return_list.append(tickers.tickers[stock_name].info)
+        except requests.exceptions.ConnectionError:
+            return_list.append({
+                'error': 2,
+            })
+        except requests.exceptions.HTTPError:
+            return_list.append({
+                'error': 3,
+            })
 
-    return jsonify(ticker)
+    return jsonify(return_list)
 
 #debug
-print(dir(yf.Ticker('9988.hk')))
-print('-'*8)
-print(yf.Ticker('9988.hk').financials)
+# print(dir(yf.Ticker('9988.hk')))
+# print('-'*8)
+# print(yf.Ticker('9988.hk').financials)
