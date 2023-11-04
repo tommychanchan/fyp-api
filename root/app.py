2520ca5b18ba84f2560cc665622535df299a80b2
tommychanchan <41499833+tommychanchan@users.noreply.github.com>
2023-11-04 23:52:15 +0800

    answer qa

--- root/app.py
+++ root/app.py
@@ -4,7 +4,21 @@ from flask import Flask, jsonify, request
 import urllib.request
 import requests
 from bs4 import BeautifulSoup
+import pymongo
 from utils import *
+
+
+
+
+# connect to database
+db_client = pymongo.MongoClient('localhost', 27017)
+fyp_db = db_client['fyp']
+qa_col = fyp_db['qa']
+
+
+
+
+
 
 app = Flask(__name__)
 
@@ -24,7 +38,7 @@ def index():
 
 # get stock information
 # -- parameters --
-# stock: the stock id (e.g. "9988.hk")
+# stocks: a list of stock ids (e.g. ["9988.hk", "0008.hk"])
 # -- return --
 # a list of stock info
 # -- error messages --
@@ -53,6 +67,7 @@ def stock_info():
     cookie_list = [
         'AADetailChart=P%7c6%2cT%7c1%2cV%7ctrue%2cB%7c3%2cD%7c1%2cDP%7c10%7e20%7e50%7e100%7e150%2cL1%7c2%7e14%2cL2%7c3%7e12%7e26%7e9%2cL3%7c12%2cCO%7c1%2cCT%7c%2cCS%7c%2cSP%7c%2cAHFT%7ctrue; DetailChartDisplay=3; MasterSymbol={stock_name}; LatestRTQuotedStocks={stock_name}; CookiePolicyCheck=0; __utmc=177965731; __utmz=177965731.1698675532.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmc=81143559; __utmz=81143559.1698675532.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); _ga=GA1.1.1497994407.1698675532; NewChart=Mini_Color=1; __utma=177965731.377915610.1698675532.1698675532.1698677691.2; __utma=81143559.2140254432.1698675532.1698675532.1698677691.2; AALTP=1; aa_cookie=158.132.155.19_49311_1698681158; __utmt_a3=1; __utmb=177965731.11.10.1698677691; __utmt_a2=1; __utmt_b=1; __utmb=81143559.22.10.1698677691; _ga_FL2WFCGS0Y=GS1.1.1698677686.2.1.1698679752.0.0.0; _ga_38RQTHE076=GS1.1.1698677686.2.1.1698679752.0.0.0',
         'aa_cookie=42.2.152.85_54005_1698895454; CookiePolicyCheck=0; _ga=GA1.1.260176329.1698899757; AALTP=1; MasterSymbol={stock_name}; LatestRTQuotedStocks={stock_name}; NewChart=Mini_Color=1; _ga_FL2WFCGS0Y=GS1.1.1698899757.1.1.1698899828.0.0.0; _ga_38RQTHE076=GS1.1.1698899757.1.1.1698899828.0.0.0',
+        'aa_cookie=44.236.48.177_30931_1699012196; mLang=TC; CookiePolicyCheck=0; _ga=GA1.1.1282790483.1699016505; __utma=177965731.1282790483.1699016505.1699016505.1699016505.1; __utmc=177965731; __utmz=177965731.1699016505.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmt_a3=1; AALTP=1; _ga_MW096YVQH9=GS1.1.1699016514.1.0.1699016522.0.0.0; MasterSymbol={stock_name}; LatestRTQuotedStocks={stock_name}; NewChart=Mini_Color=1; AAWS2=; __utmb=177965731.4.10.1699016505; __utma=81143559.1282790483.1699016505.1699016523.1699016523.1; __utmc=81143559; __utmz=81143559.1699016523.1.1.utmcsr=aastocks.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmt_a2=1; __utmt_b=1; __utmb=81143559.2.10.1699016523; _ga_FL2WFCGS0Y=GS1.1.1699016504.1.1.1699016522.0.0.0; _ga_38RQTHE076=GS1.1.1699016504.1.1.1699016522.0.0.0',
     ]
 
     for stock_name, symbol in zip(data, symbols):
@@ -195,6 +210,53 @@ def stock_info():
 
 
     return jsonify(return_list)
+
+
+
+
+# get answer of qa
+# -- parameters --
+# qs: a list of questions
+# -- return --
+# a list of qas
+# -- error messages --
+# 1: no qs provided
+# -- error messages in each element of the return list --
+# 2: question not found
+
+# for api test
+# localhost:5000/qa
+# {"qs": ["按金", "按盤價"]}
+@app.route('/qa', methods=['POST'])
+def qa():
+    json_data = request.json
+    qs = json_data['qs']
+
+
+    if len(qs) == 0:
+        return jsonify({
+            'error': 1,
+        })
+
+    return_list = []
+
+    result = None
+
+    for q in qs:
+        result = qa_col.find_one({'q': q}, {'_id': 0})
+        if result:
+            return_list.append(result)
+        else:
+            return_list.append({
+                'q': q,
+                'error': 2,
+            })
+
+    return jsonify(return_list)
+
+
+
+
 
 #debug (if here stop, server will hang)
 # print(yf_to_aa('9988.hk'))
