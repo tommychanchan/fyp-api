028153eaf3210e52c112b0690455c0b481804864
tommychanchan <41499833+tommychanchan@users.noreply.github.com>
2024-02-03 19:28:38 +0800

    code to handle scroll of aastocks in table of fundamental analysis

--- test.py
+++ test.py
@@ -1,41 +1,39 @@
 import requests
 import json
+from bs4 import BeautifulSoup
 
 from global_var import *
 
-import nasdaqdatalink as nasdaq
-nasdaq.ApiConfig.api_key = apis['nasdaq']
-data = nasdaq.get('HKEX/00607')
-data['Close'] = data['Previous Close'].shift(-1)
-data.dropna(subset = ['Close'], inplace=True)
-data['Adj Close'] = data['Close']
-url = 'http://localhost:5000/stock_split'
-headers = {}
-payload = {'stock': '0607.hk'}
+
+
+
+
+url = f'http://www.aastocks.com/tc/stocks/analysis/peer.aspx?symbol=09988'
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Cookie': 'aa_cookie=1.65.150.95_64710_1706931042; MasterSymbol=09988; LatestRTQuotedStocks=09988; _ga=GA1.1.962392854.1706935565; __utma=177965731.962392854.1706935565.1706935566.1706935566.1; __utmc=177965731; __utmz=177965731.1706935566.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt_a3=1; __utma=81143559.962392854.1706935565.1706935566.1706935566.1; __utmc=81143559; __utmz=81143559.1706935566.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt_a2=1; __utmt_b=1; CookiePolicyCheck=0; __utmb=177965731.2.10.1706935566; __utmb=81143559.4.10.1706935566; _ga_FL2WFCGS0Y=GS1.1.1706935565.1.1.1706935604.0.0.0; _ga_38RQTHE076=GS1.1.1706935565.1.1.1706935604.0.0.0',
+    'Host': 'www.aastocks.com',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+}
 try:
-    result = requests.post(
-        url, timeout=40, headers=headers, json=payload, verify=False
+    result = requests.get(
+        url, timeout=40, headers=headers, verify=False
     ).text
-    result_json = json.loads(result)
-    if type(result_json) != list and result_json.get('error'):
-        print(f'error: {result_json.get("error")}')
-    split_dividend_list = sorted(result_json, key=lambda d: d['date'], reverse=True)
-    for datum in split_dividend_list:
-        if len(data.loc[:datum['date']]) > 0 and datum['splitDividend'] == 'split':
-            data.loc[:datum['date'], ['Nominal Price', 'Adj Close']] *= datum['rate']
-            data.drop(data.loc[:datum['date']].index[-1], inplace=True)
-        elif len(data.loc[:datum['date']]) > 0 and datum['splitDividend'] == 'dividend':
-            if datum['date'] == str(data.loc[:datum['date']].iloc[-1].name)[:10]:
-                last_date = data.loc[:datum['date']].iloc[-2].name
-            else:
-                last_date = data.loc[:datum['date']].iloc[-1].name
-            nominal_price = float(data.loc[last_date, 'Nominal Price'])
-            price_rate = 1-(datum['rate']/nominal_price)
-            data.loc[:last_date, 'Adj Close'] *= price_rate
-            data.drop(last_date, inplace=True)
 
+    temp_list = json.loads((result[result.index('tsData')+8:result.index('];', result.index('tsData'))+1]).strip().replace('d0', '"d0"').replace('d1', '"d1"').replace('d2', '"d2"').replace('d3', '"d3"').replace('d4', '"d4"').replace('d5', '"d5"').replace('d6', '"d6"').replace('d7', '"d7"').replace('d8', '"d8"').replace('d9', '"d9"').replace('"d1"0', '"d10"'))
+    print('Length:', len(temp_list))
+    temp_soup = None
+    for datum in temp_list:
+        temp_soup = BeautifulSoup(datum['d0'], features='html.parser')
+        stock_id = temp_soup.find('a').text[:-3]
+        pe_ratio = datum['d6']  # 市盈率: can be "N/A"/"無盈利"/"33.06"...
+        pb_ratio = datum['d7']  # 市賬率: can be "N/A"/"3.43"...
+        print(f'{stock_id}: {pe_ratio} & {pb_ratio}')
 
-    data.iloc[:].to_csv('test.csv', sep=',', encoding='utf-8')
 except requests.exceptions.ConnectionError as e:
-    print(f'ERROR({payload}): {e}')
-
+    print(f'ERROR: {e}')
